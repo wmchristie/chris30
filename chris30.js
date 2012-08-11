82c9908dfb97a1ab0213f6ee0a06e19a0e7b83a9
William Christie <william.christie@gmail.com>
2012-08-11 16:21:14 -0400

    progress

---
+++ chris30.js
@@ -0,0 +1,49 @@
+(function ($) {
+
+    $(function () {
+
+        $('body').on('click', 'a[href="#"]', function (e) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            var target = $('#' + $(e.target).data('id'));
+            
+            $('.popup.visible').removeClass('visible');
+            target.addClass('visible');
+
+            var pwidth = target.width(),
+                pheight = target.height(),
+                bwidth = $(window).width(),
+                bheight = $(window).height(); 
+
+                console.log(bheight / 2 - pheight / 2, $(document).scrollTop());
+
+
+            target.css({
+                top : (bheight / 2 - pheight / 2) + $(document).scrollTop(),
+                left : (bwidth / 2 - pwidth / 2) + $(document).scrollLeft()
+            });
+
+        });
+
+        $('body').on('click', '.controls button', function (e) {
+            
+            e.preventDefault();
+            e.stopPropagation();
+
+            $(e.target).closest('.popup').removeClass('visible');
+
+        });
+
+        $(document).on('click', 'body', function (e) {
+
+            if ($(e.target).closest('.popup').size() === 0) {
+                $('.popup.visible').removeClass('visible');
+            }
+
+        });
+
+    });
+
+}(jQuery));
